26554d4b004112814ed8eee70b57570ade94ab12
bharathjinka09 <bharathjinka09@gmail.com>
2022-01-29 15:41:01 +0530

    Getting the submitted data in json format jquery steps form

--- script.js
+++ script.js
@@ -23,6 +23,26 @@ form.children("div").steps({
     },
     onFinished: function (event, currentIndex)
     {
-        alert("Submitted!");
+        // alert("Submitted!");
+
+        // json data
+        var data = JSON.stringify( $(form).serializeArray() );
+        console.log({data})
+        
+        // js object
+        var parsedData = JSON.parse(data);
+        console.log({parsedData})
+
+        for (var key in parsedData) {
+           console.log(key);
+           console.log(parsedData[key]);
+           console.log(parsedData[key]['name'],"only key");
+           console.log(parsedData[key]['value'],"only value");
+       }
+        // console.log(form.serialize())
+        
+        // js object
+        console.log($(form).serializeArray(),"$(form).serializeArray()")
+        // console.log(form)
     }
 });
